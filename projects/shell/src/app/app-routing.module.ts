f8c888ca2d4a9a727ba82752da352fbf3e1ed272
John Albert <jpalbert81@gmail.com>
2022-11-30 10:17:30 -0800

    add routes

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('admin/Module').then((m) => m.RemoteEntryModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('dashboard/Module').then((m) => m.RemoteEntryModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
