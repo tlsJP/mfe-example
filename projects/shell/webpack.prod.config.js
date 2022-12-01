62b0f9dbe4975f5bf9f76379db3017bd6a0f3d03
John Albert <jpalbert81@gmail.com>
2022-12-01 05:31:05 -0800

    ghetto prd override

--- projects/shell/webpack.prod.config.js
+++ projects/shell/webpack.prod.config.js
@@ -1,1 +1,73 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, "../../tsconfig.json"), [
+  /* mapped paths to share */
+]);
+module.exports = {
+  output: {
+    uniqueName: "shell",
+    publicPath: "auto",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      library: { type: "module" },
+
+      // For remotes (please adjust)
+      // name: "shell",
+      // filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './projects/shell/src/app/app.component.ts',
+      // },
+
+      // For hosts (please adjust)
+      remotes: {
+        admin: "https://mfeadmin.z13.web.core.windows.net/remoteEntry.js",
+        dashboard:
+          "https://mfedashboard.z13.web.core.windows.net/remoteEntry.js",
+        randomApp:
+          "https://victorious-wave-0753d551e.2.azurestaticapps.net/remoteEntry.js",
+      },
+
+      shared: share({
+        "@angular/core": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/common/http": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+        "@angular/router": {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: "auto",
+        },
+
+        ...sharedMappings.getDescriptors(),
+      }),
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
