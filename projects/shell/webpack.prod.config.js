31c3bd0aaa151684505661c373bb73cd41e39d38
John Albert <jpalbert81@gmail.com>
2022-12-01 05:57:56 -0800

    fix remotes

--- projects/shell/webpack.prod.config.js
+++ projects/shell/webpack.prod.config.js
@@ -36,8 +36,10 @@ module.exports = {
 
       // For hosts (please adjust)
       remotes: {
-        admin: "https://mango-rock-0c0243e1e.2.azurestaticapps.net",
-        dashboard: "https://icy-moss-0f952f31e.2.azurestaticapps.net",
+        admin:
+          "https://mango-rock-0c0243e1e.2.azurestaticapps.net/remoteEntry.js",
+        dashboard:
+          "https://icy-moss-0f952f31e.2.azurestaticapps.net/remoteEntry.js",
         randomApp:
           "https://victorious-wave-0753d551e.2.azurestaticapps.net/remoteEntry.js",
       },
