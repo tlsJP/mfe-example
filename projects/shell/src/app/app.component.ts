72235aa19beeddea0db04132453505e40fc9ed3b
John Albert <jpalbert81@gmail.com>
2023-05-28 13:10:03 -0700

    stubbing login and logout

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
+import { Auth, User, getAuth, signOut, user } from '@angular/fire/auth';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,26 @@ import { FirebaseApp } from '@angular/fire/app';
 })
 export class AppComponent {
   title = 'shell';
-  constructor(defaultApp: FirebaseApp) {}
+  
+  private auth: Auth = getAuth();
+  user$ = user(this.auth);  
+  userSubscription: Subscription;
+  user: User |null = null;
+
+  constructor(defaultApp: FirebaseApp) {
+  
+    this.userSubscription = this.user$.subscribe(aUser => {
+        //handle user state changes here. Note, that user will be null if there is no currently logged in user.
+     console.log(aUser);
+     this.user = aUser;
+    })
+    
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+  logout(){
+    signOut(this.auth);
+  }
 }
