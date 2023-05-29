a719515ca6e7b5d0746a847399c9ecd77adf06b8
John Albert <jpalbert81@gmail.com>
2023-05-28 18:15:01 -0700

    update naming

--- projects/shell/src/environments/environment.prd.ts
+++ projects/shell/src/environments/environment.prd.ts
@@ -10,5 +10,5 @@ export const environment = {
   production: true,
   adminUrl: 'http://localhost:5001/remoteEntry.js',
   dashboardUrl: 'http://localhost:5002/remoteEntry.js',
-  remoteUrl: 'http://localhost:3030/remoteEntry.js',
+  amogusUrl: 'http://localhost:3030/remoteEntry.js',
 };
