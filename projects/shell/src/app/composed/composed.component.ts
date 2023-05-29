27e6ede01ef38a1ffd169a07b7c3cf38b44fa290
John Albert <jpalbert81@gmail.com>
2023-05-28 20:38:25 -0700

    set breakpoints

--- projects/shell/src/app/composed/composed.component.ts
+++ projects/shell/src/app/composed/composed.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-composed',
   templateUrl: './composed.component.html',
-  styleUrls: ['./composed.component.css']
+  styleUrls: ['./composed.component.css'],
 })
-export class ComposedComponent {
+export class ComposedComponent implements OnInit {
+  breakpoint!: number;
 
+  ngOnInit(): void {
+    this.breakpoint = window.innerWidth < 800 ? 5 : 10;
+  }
+
+  onResize(event: any) {
+    this.breakpoint = event!.target.innerWidth <= 800 ? 5 : 10;
+  }
 }
